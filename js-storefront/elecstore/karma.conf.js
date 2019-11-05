f6cfccac85d006a83fa5009579979edf81a656d0
Yuping Shi <yuping.shi@sap.com>
2019-11-05 20:24:20 +0800

    use pure angular

--- js-storefront/elecstore/karma.conf.js
+++ js-storefront/elecstore/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/mystore'),
+      dir: require('path').join(__dirname, './coverage/elecstore'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- js-storefront/elecstore/src/app/app.component.spec.ts
+++ js-storefront/elecstore/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mystore'`, () => {
+  it(`should have as title 'elecstore'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mystore');
+    expect(app.title).toEqual('elecstore');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mystore!');
+    expect(compiled.querySelector('.content span').textContent).toContain('elecstore app is running!');
   });
 });
